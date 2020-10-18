81a860351cf0ab1e70f04c1c50affaf5fb3759db
Matthias Brown <8matthias.brown8@gmail.com>
2020-10-18 16:42:54 +0200

    [network/note] Add NoteRequest

---
+++ app/src/main/java/fr/notes/core/note/NoteRequest.java
@@ -0,0 +1,9 @@
+package fr.notes.core.note;
+
+import java.time.LocalDateTime;
+
+public class NoteRequest {
+    public String title;
+    public String content;
+    public LocalDateTime created;
+}
