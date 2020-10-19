58b8af3ba500a5cc35228f47050ec89544407016
Matthias Brown <8matthias.brown8@gmail.com>
2020-10-20 01:46:48 +0200

    [ui/note] Handle fragment backstack transactions

--- app/src/main/java/fr/notes/views/MainActivity.java
+++ app/src/main/java/fr/notes/views/MainActivity.java
@@ -2,28 +2,26 @@ package fr.notes.views;
 
 import android.os.Bundle;
 
-import androidx.appcompat.widget.SwitchCompat;
-import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.google.android.material.appbar.MaterialToolbar;
 import com.squareup.otto.Subscribe;
 
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.CheckedChange;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
-import org.androidannotations.annotations.ViewById;
+
+import java.util.LinkedList;
 
 import javax.inject.Inject;
 
 import fr.notes.App;
 import fr.notes.R;
 import fr.notes.injects.base.BaseActivity;
+import fr.notes.injects.base.BaseFragment;
 import fr.notes.injects.bus.AppBus;
-import fr.notes.utils.AppThemeUtils;
 import fr.notes.utils.Logs;
-import fr.notes.utils.Prefs;
+import fr.notes.views.events.PopFragmentEvent;
 import fr.notes.views.events.ShowFragmentEvent;
 
 @EActivity(R.layout.act_main)
@@ -34,10 +32,9 @@ public class MainActivity extends BaseActivity {
     @Inject
     protected LoginViewModel loginViewModel;
 
-    @ViewById
-    protected Toolbar tlbMain;
-
     protected Bundle lastSavedInstanceState;
+
+    private LinkedList<Fragment> fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,23 +52,21 @@ public class MainActivity extends BaseActivity {
     @AfterViews
     public void bind() {
 
-        Prefs.setPrefEnableDarkMode(appContext, true);
-
-        AppThemeUtils.enableDarkMode(true);
-
-        setSupportActionBar(tlbMain);
-        getSupportActionBar().setTitle(getString(R.string.common_title_notes));
-
         displayFirstFragment();
     }
 
     public void displayFirstFragment() {
+        fragments = new LinkedList<>();
 
         if (lastSavedInstanceState == null) {
+
             ShowFragmentEvent event = new ShowFragmentEvent(MainFragment_.builder().build());
-            event.replace = false;
+            event.replace = true;
             event.addToBackStack = true;
             showFragment(event);
+        } else {
+            Logs.debug(this, "[DEBUG] not first instance state: " + fragments.size());
+            fragments = new LinkedList<>(getSupportFragmentManager().getFragments());
         }
     }
 
@@ -81,6 +76,19 @@ public class MainActivity extends BaseActivity {
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
+        if (!fragments.isEmpty()) {
+            fragments.getLast().onPause();
+        }
+
+        if (event.animations == null) {
+            transaction.setCustomAnimations(
+                    R.anim.slide_enter,
+                    R.anim.slide_exit,
+                    R.anim.slide_pop_enter,
+                    R.anim.slide_pop_exit
+            );
+        }
+
         if (event.replace) {
             transaction.replace(R.id.fragment_container, event.fragment);
         } else {
@@ -89,16 +97,31 @@ public class MainActivity extends BaseActivity {
 
         if (event.addToBackStack) {
             transaction.addToBackStack(event.fragmentName);
+            fragments.addLast(event.fragment);
         } else {
             transaction.addToBackStack(null);
+        }
+
+        if (event.animations != null) {
+            Logs.debug(this, String.format("%s:%s", event.animations[0], R.anim.slide_enter));
+            transaction.setCustomAnimations(event.animations[0], event.animations[1]);
         }
 
         transaction.commit();
     }
 
-    @CheckedChange(R.id.swtTheme)
-    public void switchTheme(boolean isChecked) {
-        Logs.debug(this, "[DEBUG] switch");
+    @Subscribe
+    @UiThread(propagation = UiThread.Propagation.REUSE)
+    public void popFragment(PopFragmentEvent event) {
+        if (fragments.size() > 1) {
+            fragments.removeLast();
+            getSupportFragmentManager().popBackStackImmediate();
+
+            if (!fragments.isEmpty()) {
+                fragments.getLast().onResume();
+            }
+        }
+    }
 
         Prefs.setPrefEnableDarkMode(appContext, isChecked);
 
