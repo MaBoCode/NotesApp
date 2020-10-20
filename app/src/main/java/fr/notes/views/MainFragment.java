9067e05e820121c396307c1bd13f2eebded1712e
Matthias Brown <8matthias.brown8@gmail.com>
2020-10-20 02:03:24 +0200

    [ui/note] Add toolbar and theme switch

--- app/src/main/java/fr/notes/views/MainFragment.java
+++ app/src/main/java/fr/notes/views/MainFragment.java
@@ -3,13 +3,14 @@ package fr.notes.views;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.transition.TransitionInflater;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.CheckedChange;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
-import org.androidannotations.annotations.res.StringRes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,9 @@ import fr.notes.App;
 import fr.notes.R;
 import fr.notes.injects.base.BaseFragment;
 import fr.notes.models.NoteModel;
+import fr.notes.utils.AppThemeUtils;
 import fr.notes.utils.Logs;
+import fr.notes.utils.Prefs;
 import fr.notes.views.events.ShowFragmentEvent;
 import fr.notes.views.notes.NoteDetailsFragment_;
 import fr.notes.views.notes.NotesListView;
@@ -27,18 +30,22 @@ import fr.notes.views.notes.NotesListView;
 public class MainFragment extends BaseFragment {
 
     @ViewById
+    protected Toolbar tlbMain;
+    @ViewById
+    protected SwitchCompat swtTheme;
+
+    @ViewById
     protected NotesListView viewNotes;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TransitionInflater inflater = TransitionInflater.from(uiContext);
-        setEnterTransition(inflater.inflateTransition(R.transition.slide_right));
     }
 
     @AfterViews
     public void init() {
+
+        tlbMain.setTitle(getString(R.string.app_name));
         display();
     }
 
@@ -49,8 +56,7 @@ public class MainFragment extends BaseFragment {
 
     @Override
     public void setTransitions() {
-        TransitionInflater inflater = TransitionInflater.from(uiContext);
-        setEnterTransition(inflater.inflateTransition(R.transition.fade));
+
     }
 
     @Click(R.id.btnNewNote)
@@ -58,12 +64,12 @@ public class MainFragment extends BaseFragment {
         ShowFragmentEvent event = new ShowFragmentEvent(NoteDetailsFragment_.builder().build());
         event.replace = true;
         event.addToBackStack = true;
+
         bus.post(event);
     }
 
     public void display() {
         List<NoteModel> notes = new ArrayList<>();
-        Logs.debug(this, "content: " + getString(R.string.default_note_content));
         notes.add(new NoteModel("Tasks", getString(R.string.default_note_content), "Sun, 8:00"));
         notes.add(new NoteModel("Balance", "Some content", "Sun, 8:00"));
         notes.add(new NoteModel("Tasks", getString(R.string.default_note_content), "Sun, 8:00"));
@@ -83,5 +89,19 @@ public class MainFragment extends BaseFragment {
         if (!isDetached()) {
             viewNotes.bind(notes);
         }
+    }
+
+    @CheckedChange(R.id.swtTheme)
+    public void switchTheme(boolean isChecked) {
+        Logs.debug(this, "[checked]" + isChecked);
+
+        Prefs.setPrefEnableDarkMode(appContext, isChecked);
+
+        AppThemeUtils.enableDarkMode(isChecked);
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        return true;
     }
 }
