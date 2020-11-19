cb7a57a658a8bef185284f59aa36edc858a071d3
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-19 04:58:35 +0100

    [webservices/note] Refactor Note model

--- app/src/main/java/fr/notes/core/note/Note.java
+++ app/src/main/java/fr/notes/core/note/Note.java
@@ -8,28 +8,12 @@ public class Note implements Serializable {
     public String id;
     public String title;
     public String content;
-    public String timestamp;
-    public LocalDateTime created;
-    public Integer lineCount;
-
-    public Integer countContentLines() {
-        int lineCount = 0;
-        for (int i = 0; i < content.length(); i++) {
-            char c = content.charAt(i);
-            if (c == '\n')
-                lineCount++;
-        }
-        return lineCount;
-    }
 
     @Override
     public String toString() {
         return "Note{" +
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
-                ", timestamp='" + timestamp + '\'' +
-                ", creationDate=" + created +
-                ", lineCount=" + lineCount +
                 '}';
     }
 }
