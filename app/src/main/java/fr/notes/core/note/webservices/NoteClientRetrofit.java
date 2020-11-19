48a9022f69554a82fb13891afd69abb491d5ae9e
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-19 04:57:48 +0100

    [webservices/note] Add NoteClientRetrofit

---
+++ app/src/main/java/fr/notes/core/note/webservices/NoteClientRetrofit.java
@@ -0,0 +1,32 @@
+package fr.notes.core.note.webservices;
+
+import android.content.Context;
+
+import java.util.List;
+
+import fr.notes.core.note.Note;
+import fr.notes.injects.bus.AppBus;
+import fr.notes.utils.Logs;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+
+public class NoteClientRetrofit implements NoteClient {
+
+    private AppBus bus;
+    private Retrofit retrofit;
+    private Context context;
+    private NoteService noteService;
+
+    public NoteClientRetrofit(AppBus bus, Retrofit retrofit, Context context) {
+        this.bus = bus;
+        this.retrofit = retrofit;
+        this.noteService = retrofit.create(NoteService.class);
+        this.context = context;
+    }
+
+    @Override
+    public void loadNotes() {
+        Call<List<Note>> notes = noteService.getNotes(0);
+        Logs.debug(this, notes.toString());
+    }
+}
