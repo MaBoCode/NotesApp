84a796863058dff27c8e0ce13cb046d101fe74a1
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-20 16:31:48 +0100

    [network/note] refactor NoteViewModel

--- app/src/main/java/fr/notes/core/note/NoteViewModel.java
+++ app/src/main/java/fr/notes/core/note/NoteViewModel.java
@@ -1,51 +1,50 @@
 package fr.notes.core.note;
 
+import androidx.hilt.Assisted;
 import androidx.hilt.lifecycle.ViewModelInject;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
+import fr.notes.core.events.BaseStateEvent;
+import fr.notes.core.events.GetNotesStateEvent;
 import fr.notes.core.note.repositories.NoteRepository;
-import fr.notes.utils.Logs;
+import fr.notes.core.util.DataState;
 
 public class NoteViewModel extends ViewModel {
 
-    protected NoteRepository noteRepository;
-
     protected Long userId = 1L;
 
-    protected MutableLiveData<List<Note>> notes;
+    private final NoteRepository noteRepository;
+    private final SavedStateHandle savedStateHandle;
+
+    protected MutableLiveData<DataState<List<Note>>> dataState = new MutableLiveData<>();
 
     @ViewModelInject
-    public NoteViewModel(NoteRepository noteRepository) {
+    public NoteViewModel(NoteRepository noteRepository, @Assisted SavedStateHandle savedStateHandle) {
         this.noteRepository = noteRepository;
-        notes = noteRepository.fetchNotes(userId);
+        this.savedStateHandle = savedStateHandle;
     }
 
-    public void loadNotes() {
-        MutableLiveData<List<Note>> fetchedNotes = noteRepository.fetchNotes(userId);
-        notes.setValue(fetchedNotes.getValue());
-    }
-
-    public void saveNote(String title, String content) {
-        List<Note> rawNotes = notes.getValue();
-        Note note = noteRepository.saveNote(userId, title, content).getValue();
-
-        if (rawNotes != null && note != null) {
-            rawNotes.add(note);
-            notes.setValue(rawNotes);
+    public void setStateEvent(BaseStateEvent event) {
+        if (event instanceof GetNotesStateEvent) {
+            DataState<List<Note>> newDataState = noteRepository.fetchNotes(userId);
+            dataState.setValue(newDataState);
         }
     }
 
-    public void editNote(Long noteId, String title, String content) {
-        noteRepository.editNote(userId, noteId, title, content);
-        loadNotes();
+    /*
+    @Subscribe
+    public void getNotes(GetNotesStateEvent event) {
+        DataState<List<Note>> newDataState = noteRepository.fetchNotes(userId);
+        dataState.setValue(newDataState);
     }
 
-    public LiveData<List<Note>> getNotes() {
-        return notes;
+     */
+
+    public MutableLiveData<DataState<List<Note>>> getDataState() {
+        return dataState;
     }
 }
