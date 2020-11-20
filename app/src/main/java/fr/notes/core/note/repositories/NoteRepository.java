bdcbfedc30397e6caf940f4bb6541f69cf8ca9ff
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-20 16:31:23 +0100

    [network/note] refactor NoteRepository

--- app/src/main/java/fr/notes/core/note/repositories/NoteRepository.java
+++ app/src/main/java/fr/notes/core/note/repositories/NoteRepository.java
@@ -1,44 +1,46 @@
 package fr.notes.core.note.repositories;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
 import java.util.List;
 
 import fr.notes.core.note.Note;
+import fr.notes.core.note.cache.NoteCacheEntity;
 import fr.notes.core.note.cache.NoteDao;
 import fr.notes.core.note.util.NoteCacheEntityMapper;
 import fr.notes.core.note.util.NoteEntityMapper;
+import fr.notes.core.note.webservices.NoteEntity;
 import fr.notes.core.note.webservices.NoteService;
+import fr.notes.core.util.DataState;
 
 public class NoteRepository {
 
-    protected NoteService noteService;
     protected NoteDao noteDao;
+    protected NoteService noteService;
     protected NoteCacheEntityMapper cacheEntityMapper;
     protected NoteEntityMapper entityMapper;
 
-    protected MutableLiveData<List<Note>> notes = new MutableLiveData<>();
-    protected MutableLiveData<Note> note = new MutableLiveData<>();
-
     public NoteRepository(NoteDao noteDao, NoteService noteService, NoteCacheEntityMapper cacheEntityMapper, NoteEntityMapper entityMapper) {
-        this.noteService = noteService;
         this.noteDao = noteDao;
+        this.noteService = noteService;
         this.cacheEntityMapper = cacheEntityMapper;
         this.entityMapper = entityMapper;
     }
 
-    public MutableLiveData<List<Note>> fetchNotes(Long userId) {
+    public DataState<List<Note>> fetchNotes(Long userId) {
+        try {
+            List<NoteEntity> entities = noteService.getNotes(userId);
+            List<Note> notes = entityMapper.mapFromEntities(entities);
 
-        return notes;
+            for (Note note: notes) {
+                NoteCacheEntity cacheEntity = cacheEntityMapper.mapToEntity(note);
+                noteDao.insert(cacheEntity);
+            }
+
+            List<NoteCacheEntity> cachedNotes = noteDao.getAll();
+            notes = cacheEntityMapper.mapFromEntities(cachedNotes);
+            return new DataState.Success<>(notes);
+        } catch (Exception e) {
+            return new DataState.Error<>(e);
+        }
     }
 
-    public LiveData<Note> saveNote(Long userId, String title, String content) {
-
-        return note;
-    }
-
-    public void editNote(Long userId, Long noteId,  String title, String content) {
-
-    }
 }
