69e310b10701e4f58bf13b6a68bf39865cc06cee
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-20 16:31:35 +0100

    [network/note] refactor NoteService

--- app/src/main/java/fr/notes/core/note/webservices/NoteService.java
+++ app/src/main/java/fr/notes/core/note/webservices/NoteService.java
@@ -2,9 +2,7 @@ package fr.notes.core.note.webservices;
 
 import java.util.List;
 
-import fr.notes.core.note.Note;
 import fr.notes.core.note.NoteRequest;
-import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -14,18 +12,18 @@ import retrofit2.http.Path;
 public interface NoteService {
 
     @GET("users/{userId}/notes")
-    List<Note> getNotes(
+    List<NoteEntity> getNotes(
             @Path("userId") Long userId
     );
 
     @POST("users/{userId}/notes")
-    Note addNote(
+    NoteEntity addNote(
             @Path("userId") Long userId,
             @Body NoteRequest request
     );
 
     @PUT("users/{userId}/notes/{noteId}")
-    Note updateNote(
+    NoteEntity updateNote(
             @Path("userId") Long userId,
             @Path("noteId") Long noteId,
             @Body NoteRequest request
