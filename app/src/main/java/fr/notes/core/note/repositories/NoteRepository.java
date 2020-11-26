283662953d8a3ad79abc7a1941e38354858166da
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-26 05:58:00 +0100

    [network/note] Add executor to NoteRepository + handle addNote and editNote

--- app/src/main/java/fr/notes/core/note/repositories/NoteRepository.java
+++ app/src/main/java/fr/notes/core/note/repositories/NoteRepository.java
@@ -1,8 +1,10 @@
 package fr.notes.core.note.repositories;
 
 import java.util.List;
+import java.util.concurrent.Executor;
 
 import fr.notes.core.note.Note;
+import fr.notes.core.note.NoteRequest;
 import fr.notes.core.note.cache.NoteCacheEntity;
 import fr.notes.core.note.cache.NoteDao;
 import fr.notes.core.note.util.NoteCacheEntityMapper;
@@ -10,6 +12,8 @@ import fr.notes.core.note.util.NoteEntityMapper;
 import fr.notes.core.note.webservices.NoteEntity;
 import fr.notes.core.note.webservices.NoteService;
 import fr.notes.core.util.DataState;
+import fr.notes.utils.Logs;
+import retrofit2.Response;
 
 public class NoteRepository {
 
@@ -18,29 +22,108 @@ public class NoteRepository {
     protected NoteCacheEntityMapper cacheEntityMapper;
     protected NoteEntityMapper entityMapper;
 
-    public NoteRepository(NoteDao noteDao, NoteService noteService, NoteCacheEntityMapper cacheEntityMapper, NoteEntityMapper entityMapper) {
+    protected final Executor executor;
+
+    public interface NoteRepositoryCallback<D> {
+        void onComplete(DataState<D> result);
+    }
+
+    public NoteRepository(NoteDao noteDao, NoteService noteService,
+                          NoteCacheEntityMapper cacheEntityMapper, NoteEntityMapper entityMapper,
+                          Executor executor) {
         this.noteDao = noteDao;
         this.noteService = noteService;
         this.cacheEntityMapper = cacheEntityMapper;
         this.entityMapper = entityMapper;
+        this.executor = executor;
     }
 
-    public DataState<List<Note>> fetchNotes(Long userId) {
-        try {
-            List<NoteEntity> entities = noteService.getNotes(userId);
-            List<Note> notes = entityMapper.mapFromEntities(entities);
+    public void fetchNotes(Long userId, NoteRepositoryCallback<List<Note>> callback) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    List<NoteEntity> entities = noteService.getNotes(userId).execute().body();
 
-            for (Note note: notes) {
-                NoteCacheEntity cacheEntity = cacheEntityMapper.mapToEntity(note);
-                noteDao.insert(cacheEntity);
+                    if (entities == null) {
+                        return;
+                    }
+
+                    List<Note> notes = entityMapper.mapFromEntities(entities);
+
+                    for (Note note: notes) {
+                        NoteCacheEntity cacheEntity = cacheEntityMapper.mapToEntity(note);
+                        noteDao.insert(cacheEntity);
+                    }
+
+                    List<NoteCacheEntity> cachedNotes = noteDao.getAll();
+                    notes = cacheEntityMapper.mapFromEntities(cachedNotes);
+
+                    DataState<List<Note>> dataState = new DataState.Success<>(notes);
+                    callback.onComplete(dataState);
+                } catch (Exception e) {
+                    DataState<List<Note>> dataState = new DataState.Error<>(e);
+                    callback.onComplete(dataState);
+                }
             }
+        });
+    }
 
-            List<NoteCacheEntity> cachedNotes = noteDao.getAll();
-            notes = cacheEntityMapper.mapFromEntities(cachedNotes);
-            return new DataState.Success<>(notes);
-        } catch (Exception e) {
-            return new DataState.Error<>(e);
-        }
+    public void addNote(Long userId, String title, String content, NoteRepositoryCallback<List<Note>> callback) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    NoteRequest request = new NoteRequest();
+                    request.title = title;
+                    request.content = content;
+
+                    Response<NoteEntity> res = noteService.addNote(userId, request).execute();
+                    NoteEntity entity = res.body();
+
+                    if (entity == null) {
+                        Logs.error(this, "entity: " + res.errorBody().string());
+                        return;
+                    }
+
+                    Note note = entityMapper.mapFromEntity(entity);
+                    NoteCacheEntity cacheEntity = cacheEntityMapper.mapToEntity(note);
+                    noteDao.insert(cacheEntity);
+
+                    List<NoteCacheEntity> cachedEntities = noteDao.getAll();
+                    List<Note> notes = cacheEntityMapper.mapFromEntities(cachedEntities);
+
+                    DataState<List<Note>> dataState = new DataState.Success<>(notes);
+                    callback.onComplete(dataState);
+                } catch (Exception e) {
+                    DataState<List<Note>> dataState = new DataState.Error<>(e);
+                    callback.onComplete(dataState);
+                }
+            }
+        });
+    }
+
+    public void editNote(Long userId, Long noteId, NoteRequest request, NoteRepositoryCallback<List<Note>> callback) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    NoteEntity entity = noteService.editNote(userId, noteId, request).execute().body();
+                    Note note = entityMapper.mapFromEntity(entity);
+                    NoteCacheEntity cacheEntity = cacheEntityMapper.mapToEntity(note);
+                    noteDao.updateNote(cacheEntity);
+
+                    List<NoteCacheEntity> cachedEntities = noteDao.getAll();
+                    List<Note> notes = cacheEntityMapper.mapFromEntities(cachedEntities);
+
+                    DataState<List<Note>> dataState = new DataState.Success<>(notes);
+                    callback.onComplete(dataState);
+                } catch (Exception e) {
+                    DataState<List<Note>> dataState = new DataState.Error<>(e);
+                    callback.onComplete(dataState);
+                }
+            }
+        });
     }
 
 }
