17fe5356520df5ebbc16e5abbd0c13fed82dab42
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-26 05:46:14 +0100

    [network/note] Handle events for ViewModels

--- app/src/main/java/fr/notes/App.java
+++ app/src/main/java/fr/notes/App.java
@@ -3,16 +3,30 @@ package fr.notes;
 import android.app.Application;
 import android.content.Context;
 
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import dagger.hilt.android.HiltAndroidApp;
+import fr.notes.injects.base.BaseViewModel;
+import fr.notes.injects.bus.AppBus;
 import fr.notes.utils.AppThemeUtils;
 import fr.notes.utils.Prefs;
 
 @HiltAndroidApp
 public class App extends Application {
 
+    @Inject
+    protected AppBus bus;
+
     private static WeakReference<Context> contextWeakReference;
+    private static List<ViewModel> viewModels = new ArrayList<>();
 
     @Override
     public void onCreate() {
@@ -29,5 +43,27 @@ public class App extends Application {
 
     public void setAppTheme() {
         AppThemeUtils.enableDarkMode(Prefs.getPrefEnableDarkMode(contextWeakReference.get()));
+    }
+
+    public static <T extends ViewModel> T getViewModel(ViewModelStoreOwner owner, Class<T> tClass) {
+        T viewModel = new ViewModelProvider(owner).get(tClass);
+
+        if (!viewModels.contains(viewModel)) {
+            viewModels.add(viewModel);
+        }
+        return viewModel;
+    }
+
+    public void unregisterViewModels() {
+        for (ViewModel viewModel: viewModels) {
+            ((BaseViewModel) viewModel).unregister(bus);
+        }
+    }
+
+    @Override
+    public void onTerminate() {
+        unregisterViewModels();
+
+        super.onTerminate();
     }
 }
