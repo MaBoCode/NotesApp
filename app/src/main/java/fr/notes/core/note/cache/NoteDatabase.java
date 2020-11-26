cfb339c94d317dfa6bd549bcbc251e494f5e3e07
Matthias Brown <8matthias.brown8@gmail.com>
2020-11-26 05:51:53 +0100

    [network/note] Change db schema version

--- app/src/main/java/fr/notes/core/note/cache/NoteDatabase.java
+++ app/src/main/java/fr/notes/core/note/cache/NoteDatabase.java
@@ -2,8 +2,12 @@ package fr.notes.core.note.cache;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
-@Database(entities = {NoteCacheEntity.class}, version = 1, exportSchema = false)
+import fr.notes.utils.DateTimeConverter;
+
+@Database(entities = {NoteCacheEntity.class}, version = 3, exportSchema = false)
+@TypeConverters({DateTimeConverter.class})
 public abstract class NoteDatabase extends RoomDatabase {
 
     public final static String DB_NAME = "notes_db";
